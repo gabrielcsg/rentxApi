331f7aa384b298346290f7ecec1b1d51074812f8
Gabriel Castro <gabrielcssg@gmail.com>
2021-06-28 09:39:51 -0300

    refatoração de useCase category, add tsyringe

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
-import { routes } from './routes';
-import swaggerFile from './swagger.json';
-import './database';
+import { routes } from './routes'
+import swaggerFile from './swagger.json'
 
+import './database'
+import './shared/container'
 
-const server = express();
+const server = express()
 
-server.use(express.json());
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-server.use(routes);
+server.use(express.json())
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+server.use(routes)
 
-
-server.listen(3333, () => console.log('Server running on: 3333'));
+server.listen(3333, () => console.log('Server running on: 3333'))
