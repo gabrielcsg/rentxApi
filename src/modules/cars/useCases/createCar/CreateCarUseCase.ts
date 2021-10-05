7b53d2c6dfac8611f404a182a67a04657d7b6ebe
Gabriel Castro <gabrielcssg@gmail.com>
2021-10-05 17:51:47 -0300

    estrutura typeorm do cadastro de carros, controller e rota

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,4 +1,4 @@
-//import { inject, injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 import { AppError } from '../../../../shared/errors/AppError';
 import { Car } from '../../infra/typeorm/entities/Car';
 import { ICarsRepository } from '../../repositories/ICarsRepository';
@@ -13,10 +13,10 @@ interface IRequest {
   category_id: string;
 }
 
-//@injectable()
+@injectable()
 class CreateCarUseCase {
   constructor(
-    //@inject('CarsRepository')
+    @inject('CarsRepository')
     private carsRepository: ICarsRepository,
   ) {}
 
